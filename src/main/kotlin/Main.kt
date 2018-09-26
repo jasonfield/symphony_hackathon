9c2fd9e5bcfedc51bfbfabfa3bc6bd81a544d75c
Jason Field <jason@jasonfield.com>
2018-09-26 13:35:08 +0100

    Add simple web server to handle incoming Jenkins notifications

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,29 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.request.receiveText
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import org.apache.log4j.BasicConfigurator
-import org.slf4j.LoggerFactory
-import org.symphonyoss.client.SymphonyClientConfigID
-import java.util.HashSet
-import org.symphonyoss.symphony.clients.model.SymUser
-import org.symphonyoss.client.model.Chat
-import org.symphonyoss.symphony.clients.model.SymMessage
-import org.symphonyoss.client.SymphonyClientFactory
 import org.symphonyoss.client.SymphonyClientConfig
+import org.symphonyoss.client.SymphonyClientFactory
 import org.symphonyoss.client.events.SymEvent
-import org.symphonyoss.symphony.agent.model.V4Message
 import org.symphonyoss.symphony.clients.model.ApiVersion
-import org.symphonyoss.symphony.clients.model.SymStream
-import org.symphonyoss.symphony.pod.model.UserDetail
+import org.symphonyoss.symphony.clients.model.SymMessage
 
 
 fun main(args: Array<String>) {
-    val logger = LoggerFactory.getLogger("main")
-
     BasicConfigurator.configure()
 
+    startWebServer()
+    startSymphonyBot()
+}
+
+private fun startSymphonyBot() {
     val symphonyClientConfig = SymphonyClientConfig(true)
 
-    //Create an initialized client
     val symphony = SymphonyClientFactory.getClient(SymphonyClientFactory.TYPE.V4, symphonyClientConfig)
 
     val userFromEmail = symphony.usersClient.getUserFromEmail("jason.field@uk.bnpparibas.com")
@@ -34,10 +36,10 @@ fun main(args: Array<String>) {
     symphony.messageService.sendMessage(stream, aMessage)
 
     val datafeed = symphony.dataFeedClient.createDatafeed(ApiVersion.V4)
-    while(true) {
+    while (true) {
         val eventsFromDatafeed: MutableList<SymEvent>? = symphony.dataFeedClient.getEventsFromDatafeed(datafeed)
         eventsFromDatafeed?.forEach {
-            if(it.type == "MESSAGESENT" && it.initiator != symphony.localUser) {
+            if (it.type == "MESSAGESENT" && it.initiator != symphony.localUser) {
                 println(it.payload)
                 val msg = SymMessage()
                 msg.messageText = "You said -> ${it.payload.messageSent.messageText}"
@@ -46,4 +48,20 @@ fun main(args: Array<String>) {
         }
         print(".")
     }
+}
+
+private fun startWebServer() {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/jenkins") {
+                println("Got a message from jenkins!")
+                println(call.receiveText())
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = false)
 }
