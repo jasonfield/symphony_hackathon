27ae0d611844a4bd0189e81b40cbd50f2de27c10
Jason Field <jason@jasonfield.com>
2018-09-27 15:12:56 +0100

    Fix tests

--- src/test/kotlin/GitHubServiceTest.kt
+++ src/test/kotlin/GitHubServiceTest.kt
@@ -5,7 +5,8 @@ class GitHubServiceTest {
     @Test
     fun `can get user from changeset`() {
         val github = GitHubService()
-        val author = github.getDetailsFromChangeset("7db4d51be652f3e90e91612d5f06c4e6352295b6")
-        assertThat(author).isEqualTo("jasonfield")
+        val details = github.getDetailsFromChangeset("7db4d51be652f3e90e91612d5f06c4e6352295b6")
+        assertThat(details.author).isEqualTo("jasonfield")
+        assertThat(details.link).isEqualTo("https://api.github.com/repos/jasonfield/symphony_hackathon_test/commits/7db4d51be652f3e90e91612d5f06c4e6352295b6")
     }
 }
